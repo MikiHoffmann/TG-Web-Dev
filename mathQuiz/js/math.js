48618acbe4ba65b438fc5b0bc101d18043a95f76
Miki Hoffmann <mh.miki.hoffmann@gmail.com>
2021-12-19 18:51:37 +0100

    quiz far from finished

--- mathQuiz/js/math.js
+++ mathQuiz/js/math.js
@@ -7,11 +7,10 @@ function createQuiz() {
     createFieldsets();
 }
 const quizBackground = document.createElement('div');
-
 function createQuizDisplay() {
     document.body.appendChild(quizBackground);
     quizBackground.classList.add('quizBackground');
-    let containerTemplate = `<h1>-- Math Problem! --</h1><span id='quizRoundDisplay'></span><div id='quizQuestionDisplay' class='quizQuestionDisplay'></div><form id='quizAwnserForm' class='quizAwnserForm'></form><div id='navButtons' class='navButtons'></div>`;
+    let containerTemplate = `<h1>-- Math Problem! --</h1><form id='quizAwnserForm' class='quizAwnserForm'></form><div id='navButtons' class='navButtons'></div>`;
     let quizContainer = document.createElement('div');
     quizContainer.innerHTML = containerTemplate;
     quizContainer.classList.add('quizContainer');
@@ -20,7 +19,7 @@ function createQuizDisplay() {
 function createNavButtons() {
     const navButtons = [
         {id:'previousButton',text:'previous'},
-        {id:'checkButton',text:'check my awnser'},
+        //{id:'checkButton',text:'check my awnser'},
         {id:'nextButton',text:'next'}
     ];
     for(let i = 0; i < navButtons.length; i++) {
@@ -30,46 +29,72 @@ function createNavButtons() {
         buttonDisplay.innerHTML += navButton;
     }
 }
-let questionNumber = 1;
-let question = '10 + 5';
-let rightAwnser = '15';
-let wrong = ['1','33','-4','12'];
-wrong.push(rightAwnser);
-let allAwnsers = wrong;
+
 const alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
 
-function shuffleArray(array) {
-    let currentIndex = array.length
-    let randomIndex;
-    while (currentIndex != 0) {
-      randomIndex = Math.floor(Math.random() * currentIndex);
-      currentIndex--;
-      [array[currentIndex], array[randomIndex]] = [
-        array[randomIndex], array[currentIndex]];
+class MathProblems {
+    constructor(calculations,rightAwnsers,wrongAwnsers) {
+        this.calculations = calculations;
+        this.rightAwnsers = rightAwnsers;
+        this.wrongAwnsers = wrongAwnsers;
     }
-    return array;
-}
+};
+let questions = [];
+questions[0] = new MathProblems('10 + 5',15,[1,33,-4,12]);
+questions[1] = new MathProblems('1 + 2',3,[2,43,-6,100,7,8]);
+questions[2] = new MathProblems('6 - 2',4,[55,33,113,44]);
+   
 
-const awnserField = document.createElement('fieldset');
+const keys = questions.keys();
+let questionNumbers =[];
+let questionNumber;
+let rightAwnser = questions.rightAwnsers;
 function createFieldsets() {
-    shuffleArray(allAwnsers);
     quizAwnserForm = document.getElementById('quizAwnserForm');
-    quizAwnserForm.appendChild(awnserField);
-    awnserField.id = 'question' + questionNumber;
-    for(let i = 0; i < allAwnsers.length; i++) {
-        let allLabels = [];
-        let template = `<input class='hideRadio choice' type='radio' name='choice' value='${allAwnsers[i]}'/><span class='customRadio'>${alphabet[i]}</span><span class='awnserOption choice'>${allAwnsers[i]}</span>`;
-        let label = template;
-        allLabels = document.createElement('label');
-        allLabels.classList.add('label');
-        allLabels.innerHTML += label;
-        awnserField.appendChild(allLabels);
-    }
-    
-    quizRoundDisplay.innerText = 'question ' + questionNumber + ' of ' + wrong.length;
-    quizQuestionDisplay.innerText = question;
-    checkButton.onclick = checkAwnser;
+    let awnserField;
+    for(let k of keys){
+    questionNumber = k + 1;
+    questionNumbers.push(questionNumber);
 }
+    for(let f = 0; f < questions.length; f++) {
+        let quizRoundDisplay = document.createElement('span');
+        quizRoundDisplay.innerText = 'question ' + questionNumbers[f] + ' of ' + questions.length;
+        let awnserFields = [];
+        awnserFields = document.createElement('fieldset');
+        let fieldTemplate = `<legend id='quizQuestionDisplay' class='quizQuestionDisplay'>${questions[f].calculations}</legend>`;
+        awnserField = fieldTemplate;
+        awnserFields.innerHTML += awnserField;
+        awnserFields.id = 'question' + questionNumbers[f];
+        
+        quizAwnserForm.appendChild(quizRoundDisplay);
+        quizAwnserForm.appendChild(awnserFields);
+        let allAwnserFields = [];
+        allAwnserFields = document.querySelectorAll('fieldset');
+        awnserFields = document.getElementById('question' + questionNumbers[f]);
+        allAwnserFields += awnserFields;
+        console.log(awnserFields); // delete log later
+        questions[f].wrongAwnsers.push(questions[f].rightAwnsers)
+        let options = questions[f].wrongAwnsers;
+        console.log(options); // delete log later
+        let checkButton = document.createElement('button');
+        for (let i = 0; i < options.length; i++) {
+            let allLabels = [];
+            let template = `<input class='hideRadio choice' type='radio' name='choice' value='${options[i]}'/><span class='customRadio'>${alphabet[i]}</span><span class='awnserOption choice'>${options[i]}</span>`;
+            let label = template;
+            allLabels = document.createElement('label');
+            allLabels.classList.add('label');
+            allLabels.innerHTML += label;
+            awnserFields.appendChild(allLabels);
+            awnserFields.appendChild(checkButton);
+            checkButton.classList.add('button','checkButton');
+            checkButton.innerText = 'check my awnser';
+        
+        }
+            
+        };
+          
+    //checkButton.onclick = checkAwnser;
+    }
 
 function checkAwnser() {
     let awnserNo = document.getElementById('question' + questionNumber);
@@ -92,4 +117,17 @@ function checkAwnser() {
             correct.parentElement.classList.add('correctionAwnser');
         }
     }
+}
+
+function shuffleArray(array) {
+    let currentIndex = array.length;
+    console.log(currentIndex);
+    let randomIndex;
+    while (currentIndex != 0) {
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex--;
+      [array[currentIndex], array[randomIndex]] = [
+        array[randomIndex], array[currentIndex]];
+    }
+    return array;
 }
