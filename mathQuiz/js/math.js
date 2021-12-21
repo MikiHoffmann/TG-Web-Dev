0b014356532a2afff61ec4661a5f932a86165435
Miki Hoffmann <mh.miki.hoffmann@gmail.com>
2021-12-21 12:45:58 +0100

    quiz again

--- mathQuiz/js/math.js
+++ mathQuiz/js/math.js
@@ -5,13 +5,12 @@ function createQuiz() {
     createQuizBackground();
     createFieldsets();
     createNavButtons();
-    navigateForm();
 }
 const quizBackground = document.createElement('div');
 const quizTitle = document.createElement('h1');
 const quizContainer = document.createElement('div');
 const quizForm = document.createElement('form');
-const navButtons = document.createElement('div');
+const navButtonContainer = document.createElement('div');
 
 function createQuizBackground() {
     document.body.appendChild(quizBackground);
@@ -27,23 +26,11 @@ function createQuizBackground() {
     quizContainer.appendChild(quizForm);
     quizForm.classList.add('quizForm');
 
-    quizContainer.appendChild(navButtons);
-    navButtons.id = 'navButtons';
-    navButtons.classList.add('navButtons');
+    quizContainer.appendChild(navButtonContainer);
+    navButtonContainer.id = 'navButtonContainer';
+    navButtonContainer.classList.add('navButtonContainer');
 }
-function createNavButtons() {
-    const navButtons = [
-        {id:'previousButton',text:'previous'},
-        {id:'checkButton',text:'check my awnser'},
-        {id:'nextButton',text:'next'}
-    ];
-    for(let i = 0; i < navButtons.length; i++) {
-        let buttonTemplate = `<button id='${navButtons[i].id}' class='button'>${navButtons[i].text}</button>`;
-        let navButton = buttonTemplate;
-        const buttonDisplay = document.getElementById('navButtons');
-        buttonDisplay.innerHTML += navButton;
-    }
-}
+
 
 const alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
 class MathProblems {
@@ -56,18 +43,20 @@ class MathProblems {
 let questions = [];
 questions[0] = new MathProblems('10 + 5',15,[1,33,-4,12]);
 questions[1] = new MathProblems('1 + 2',3,[2,43,-6,100,7,8]);
-questions[2] = new MathProblems('6 - 2',4,[55,33,113,44]);
+questions[2] = new MathProblems('6 * 2',12,[22,13,23,44]);
 questions[3] = new MathProblems('6 - 2',4,[55,33,113,44]);
    
 const keys = questions.keys();
-let questionNumbers =[];
+let questionNumbers = [];
 let questionNumber;
 for(let k of keys){
     questionNumber = k + 1;
     questionNumbers.push(questionNumber);
 }
-function createFieldsets() {
+let options = '';
 
+let formFields = []
+function createFieldsets() {
     for(let i = 0; i < questions.length; i++) {
         let formField = document.createElement('fieldset');
         formField.classList.add('formField');
@@ -76,11 +65,14 @@ function createFieldsets() {
         let legend = document.createElement('legend');
 
         quizForm.appendChild(formField);
+        formFields.push(formField);
         formField.appendChild(legend);
         legend.innerHTML = `<span>question ` + questionNumbers[i] + ' of ' + questionNumbers.length + `</span><span id='quizQuestionDisplay' class='quizQuestionDisplay'>${questions[i].calculation}</span>`;
-
-        questions[i].wrongAwnsers.push(questions[i].rightAwnser)
-        let options = questions[i].wrongAwnsers;
+        options = questions[i].wrongAwnsers.map(listOptions);
+        function listOptions (value) {
+            return value;
+        }
+        options.push(questions[i].rightAwnser)
         console.log(options); // delete log later
         for (let i = 0; i < options.length; i++) {
             let allLabels = [];
@@ -92,102 +84,92 @@ function createFieldsets() {
             formField.appendChild(allLabels);
         };    
     }
+    
+    //const checkButton = document.querySelector('#checkButton');
+    //checkButton.onclick = checkAwnser();
 }
-function navigateForm() {
-    const formFields = document.getElementsByClassName('formField');
-    let allFormFields  = Array.from(formFields);
-    console.log(allFormFields);
+const navButtons = [
+    {id:'previousButton',text:'previous'},
+    {id:'nextButton',text:'next'},
+    {id:'toResultButton',text: 'show result'}
+];
+function createNavButtons() {
+    for(let i = 0; i < navButtons.length; i++) {
+        let buttonTemplate = `<button id='${navButtons[i].id}' class='button'>${navButtons[i].text}</button>`;
+        let navButton = buttonTemplate;
+        navButtonContainer.innerHTML += navButton;
+    }
+    console.log(formFields);
     let active = 0;
     let next = 1;
-    let previous = (allFormFields.length - 1);
-    for(let i = 0; i < allFormFields.length; i++) {
-        allFormFields[active].classList.add('active');
-        allFormFields[next].classList.add('next');
-        allFormFields[previous].classList.add('previous');
+    let previous = (formFields.length - 1);
+    for(let i = 0; i < formFields.length; i++) {
+        formFields[active].classList.add('active');
+        formFields[next].classList.add('next');
+        formFields[previous].classList.add('previous');
     }
-
     const previousButton = document.querySelector('#previousButton');
     const nextButton = document.querySelector('#nextButton');
-    previousButton.onclick = showPrevious();
-    nextButton.onclick = showNext();
+    const toResultButton = document.querySelector('#toResultButton');
+    toResultButton.style.display = 'none';
+    previousButton.style.display = 'none';
+    previousButton.style.d
+    previousButton.onclick = showPrevious;
+    nextButton.onclick = showNext;
+    //toResultButton.onclick = showResult;
     console.log(previousButton,nextButton);
     function showNext() {
-        if(active < allFormFields.length - 1) {
+        if(active < formFields.length - 1) {
             nextField(active + 1);
-        }
-        else {
-            nextField(0);
+            previousButton.style.display = 'block';
         }
     }
     function showPrevious() {
         if(active > 0) {
             nextField(active - 1);
-        }
-        else {
-            nextField(allFormFields.length - 1);
+            nextButton.style.display = 'block';
         }
     }
     function nextField(item) {
         active = item;
         next = active + 1;
         previous = active - 1;
-        for (let i = 0; i < allFormFields.length; i++) {
-            allFormFields[i].classList.remove('active');
-            allFormFields[i].classList.remove('next');
-            allFormFields[i].classList.remove('previous');
+        for (let i = 0; i < formFields.length; i++) {
+            formFields[i].classList.remove('active');
+            formFields[i].classList.remove('next');
+            formFields[i].classList.remove('previous');
         }
-        if (next >= allFormFields.length) {
+        if (next >= formFields.length) {
             next = 0;
+            nextButton.style.display = 'none';
+            toResultButton.style.display = 'block';
         }
         if (previous === -1) {
-            previous = (allFormFields.length - 1);
+            previous = (formFields.length - 1);
+            previousButton.style.display = 'none';
         }
-        allFormFields[active].classList.add('active');
-        allFormFields[next].classList.add('next');
-        allFormFields[previous].classList.add('previous');
+        formFields[active].classList.add('active');
+        formFields[next].classList.add('next');
+        formFields[previous].classList.add('previous');
     }
 }
-
+/*
 function checkAwnser() {
     let allOptions = document.querySelectorAll('input[name="choice"]');
     let correct = '';
     //let points = 0;
     for(let j = 0; j < allOptions.length; j++) {
-        if(allOptions[j].value === rightAwnser) {
+        if(allOptions[j].value === questions[j].rightAwnser) {
             correct = allOptions[j];
         }
-        if((allOptions[i].checked === true) && (allOptions[i].value === rightAwnser)) {
-            allOptions[i].parentElement.classList.add('awnserRight');
+        if((allOptions[j].checked === true) && (allOptions[j].value === questions[j].rightAwnser)) {
+            allOptions[j].parentElement.classList.add('awnserRight');
             //++points;
         }
-        if((allOptions[i].checked === true) && (allOptions[i].value !== rightAwnser)) {
-            allOptions[i].parentElement.classList.add('awnserWrong');
+        if((allOptions[j].checked === true) && (allOptions[j].value !== questions[j].rightAwnser)) {
+            allOptions[j].parentElement.classList.add('awnserWrong');
             correct.parentElement.classList.add('correctionAwnser');
         }
     }
-}
-/*
-    legend.appendChild(quizQuestionDisplay);
-    quizQuestionDisplay.innerText = `${questions[i].calculation}`;
-
-    
-        let awnserFields = [];
-        let fieldTemplate = ;
-        awnserField = fieldTemplate;
-        awnserFields.innerHTML += awnserField;
-        
-        quizForm.appendChild(awnserFields);
-
-        allAwnserFields = document.querySelectorAll('fieldset');
-        awnserFields = document.getElementById('question' + questionNumbers[i]);
-        allAwnserFields += awnserFields;
-
-        console.log(awnserFields); // delete log later
-
-        
-            //checkButton.onclick = checkAwnser();
-
-
- 
-*/       
-        
+}      
+      */  
